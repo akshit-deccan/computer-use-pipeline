7e00943eaa3c8b9ed4c09226690c3c19e886f2e8
Akshit Kumar <akshitdotkr@gmail.com>
2025-04-14 16:08:26 +0530

    init project

---
+++ run.sh
@@ -0,0 +1,12 @@
+# remember to populate your .env file with the key, this way:
+# ANTHROPIC_API_KEY=<your_key>
+export ANTHROPIC_API_KEY=$(grep ANTHROPIC_API_KEY .env | cut -d '=' -f2)
+docker run \
+    -e ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY \
+    -v $HOME/.anthropic:/home/computeruse/.anthropic \
+    -v $HOME/Developer/deccan/web_operators/computeruse/computer_use_demo:/home/computeruse/computer_use_demo \
+    -p 5900:5900 \
+    -p 8501:8501 \
+    -p 6080:6080 \
+    -p 8080:8080 \
+    -it ghcr.io/anthropics/anthropic-quickstarts:computer-use-demo-latest
